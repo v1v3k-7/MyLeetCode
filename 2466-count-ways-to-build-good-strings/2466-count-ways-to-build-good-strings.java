6228997fda4eb4f67d6cfa379f3611521ca7d270
Vivek <248838127+v1v3k-7@users.noreply.github.com>
2026-06-23 21:09:34 +0530

    Time: 6 ms (99.26%), Space: 45.6 MB (48.9%) - LeetHub

--- 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.java
+++ 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.java
@@ -1,20 +1,32 @@
 class Solution {
+    //Tabulation
     final int MOD=1_000_000_007;
     public int countGoodStrings(int low, int high, int zero, int one) 
     {
         int maxLen=high+Math.max(zero, one);
         int dp[]=new int[maxLen+1];
-        Arrays.fill(dp, -1);
-        return backtrack(low, high, zero, one, 0, dp);
+
+        for(int len=high; len>=0; len--)
+        {
+            long count=0;
+            if(len>=low) count++;
+            count+=dp[len+zero];
+            count+=dp[len+one];
+            dp[len]=(int)count % MOD;
+        }
+        // Arrays.fill(dp, -1);
+        // return backtrack(low, high, zero, one, 0, dp);
+        return dp[0];
+
     }
-    public int backtrack(int low, int high, int zero, int one, int len, int[] dp)
-    {
-        if(len>high) dp[len]=0;
-        if(dp[len]!=-1) return dp[len];
-        long count=0;
-        if(len>=low) count++;
-        count+=backtrack(low, high, zero, one, len+zero, dp); //len + no of zero
-        count+=backtrack(low, high, zero, one, len+one, dp); //len + no of one --> new Len in next call
-        return dp[len]=(int)count%MOD;
-    }
+    // public int backtrack(int low, int high, int zero, int one, int len, int[] dp)
+    // {
+    //     if(len>high) dp[len]=0;
+    //     if(dp[len]!=-1) return dp[len];
+    //     long count=0;
+    //     if(len>=low) count++;
+    //     count+=backtrack(low, high, zero, one, len+zero, dp); //len + no of zero
+    //     count+=backtrack(low, high, zero, one, len+one, dp); //len + no of one --> new Len in next call
+    //     return dp[len]=(int)count%MOD;
+    // }
 }
