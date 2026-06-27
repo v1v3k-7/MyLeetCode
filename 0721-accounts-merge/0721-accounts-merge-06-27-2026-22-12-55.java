3f1cd7e0f1e428dfcb89f7e1c21696e987e22a6c
Vivek <248838127+v1v3k-7@users.noreply.github.com>
2026-06-27 22:12:56 +0530

    Time: 38 ms (35.94%), Space: 50.7 MB (25.74%) - Medium

---
+++ 0721-accounts-merge/0721-accounts-merge-06-27-2026-22-12-55.java
@@ -0,0 +1,47 @@
+class Solution {
+    public List<List<String>> accountsMerge(List<List<String>> accounts) 
+    {
+        HashMap<String, List<String>> adjMap=new HashMap<>(); // mail as node, mail --> linked mails
+        HashMap<String, String> names=new HashMap<>(); //mail -> name
+        for(List<String> account: accounts)
+        {
+            int n=account.size();
+            if(!adjMap.containsKey(account.get(1))) adjMap.put(account.get(1), new ArrayList<>());
+            names.put(account.get(1), account.get(0));
+            if(n<2) continue;
+            adjMap.get(account.get(1)).addAll(account.subList(2, n));
+            for(int i=2; i<n; i++)
+            {
+                if(!adjMap.containsKey(account.get(i))) adjMap.put(account.get(i), new ArrayList<>());
+                adjMap.get(account.get(i)).add(account.get(1));
+                names.put(account.get(i), account.get(0));
+            }
+        }
+        List<List<String>> res=new ArrayList<>();
+        Set<String> visited=new HashSet<>();
+        for(String mail: adjMap.keySet())
+        {
+            if(visited.contains(mail)) continue;
+            List<String> acc=new ArrayList<>();
+            dfs(mail, adjMap, visited, acc);
+            Collections.sort(acc);
+            acc.add(0, names.get(mail));
+            res.add(acc);
+        }
+
+        return res;
+    }
+
+    public void dfs(String src, HashMap<String, List<String>> adjMap, Set<String> visited, List<String> res)
+    {
+        res.add(src);
+        visited.add(src);
+        for(String neighbour: adjMap.get(src))
+        {
+            if(!visited.contains(neighbour)) 
+            {
+                dfs(neighbour, adjMap, visited, res);
+            }
+        }
+    }
+}
