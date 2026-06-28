225f0aa35873d5b32bc743826455cb8f785df1fd
Vivek <248838127+v1v3k-7@users.noreply.github.com>
2026-06-28 21:56:30 +0530

    Added solution - LeetHub

--- 700255-topological-sort-GFG/700255-topological-sort.java
+++ 700255-topological-sort-GFG/700255-topological-sort.java
@@ -1,31 +1,37 @@
-class Solution {
+class Solution 
+{
     public ArrayList<Integer> topoSort(int V, int[][] edges) 
     {
         List<List<Integer>> adj=new ArrayList<>();
-        int[] indegree=new int[V];
         for(int i=0; i<V; i++) adj.add(new ArrayList<>());
         for(int[] edge: edges)
         {
             adj.get(edge[0]).add(edge[1]);
-            indegree[edge[1]]++;
         }
         ArrayList<Integer> res=new ArrayList<>();
-        Queue<Integer> queue=new LinkedList<>();
-        for(int i=0;i<V; i++)
+        Stack<Integer> st=new Stack<>();
+        boolean[] visited=new boolean[V];
+        for(int i=0; i<V; i++)
         {
-            if(indegree[i]==0) queue.offer(i);
-        }
-        while(!queue.isEmpty())
-        {
-            int node=queue.poll();
-            res.add(node);
-            for(int neighbour: adj.get(node))
+            if(!visited[i])
             {
-                indegree[neighbour]--;
-                if(indegree[neighbour]==0) queue.offer(neighbour);
+                dfsTopoSort(i, visited, st, adj);
             }
         }
+        for(int i=0; i<V; i++) res.add(st.pop());
         return res;
+    }
+    public void dfsTopoSort(int src, boolean[] visited, Stack<Integer> st, List<List<Integer>> adj)
+    {
+        visited[src]=true;
+        for(int neighbour: adj.get(src))
+        {
+            if(!visited[neighbour])
+            {
+                dfsTopoSort(neighbour, visited, st, adj);
+            }
+        }
+        st.push(src);
     }
 }
 
