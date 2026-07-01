c2f3003c6d45ee3064606b1b1e29684d24ac75a1
Vivek <248838127+v1v3k-7@users.noreply.github.com>
2026-07-01 10:23:41 +0530

    Time: 5 ms (77.98%), Space: 66.1 MB (69.22%) - Medium

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected-07-01-2026-10-23-39.java
@@ -0,0 +1,57 @@
+class Solution {
+    public int makeConnected(int n, int[][] connections) {
+        DisjointSet dsu=new DisjointSet(n);
+        int extraCable=0;
+        for(int[] edge: connections)
+        {
+            if(!dsu.unionBySize(edge[0], edge[1]))
+            {
+                extraCable++;
+            }
+        }
+        int need=dsu.components-1;
+        
+        if(need>extraCable) return -1;
+        else return need;
+    }
+}
+class DisjointSet
+{
+    int[] parent, size;
+    int components;
+    DisjointSet(int nodes)
+    {
+        this.parent=new int[nodes];
+        this.size=new int[nodes];
+        this.components=nodes;
+        for(int i=0; i<nodes; i++)
+        {
+            this.size[i]=1;
+            this.parent[i]=i;
+        }
+    }
+    int findRootParent(int node)
+    {
+        if(node==parent[node]) return node;
+        parent[node]=findRootParent(parent[node]);
+        return parent[node];
+    }
+    boolean unionBySize(int node1, int node2)
+    {
+        int rootParent1=findRootParent(node1);
+        int rootParent2=findRootParent(node2);
+        if(rootParent1==rootParent2) return false;
+        components--;
+        if(size[rootParent1]<size[rootParent2])
+        {
+            parent[rootParent1]=rootParent2;
+            size[rootParent2]+=size[rootParent1];
+        }
+        else
+        {
+            parent[rootParent2]=rootParent1;
+            size[rootParent1]+=size[rootParent2];
+        }
+        return true;
+    }
+}
