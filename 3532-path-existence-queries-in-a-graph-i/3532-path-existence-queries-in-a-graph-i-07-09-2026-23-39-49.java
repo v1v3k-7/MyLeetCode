0a321e3df9e522e3a28e57e42ff27041ea2c5368
Vivek <248838127+v1v3k-7@users.noreply.github.com>
2026-07-09 23:39:51 +0530

    Time: 49 ms (7.92%), Space: 263.5 MB (5.94%) - Medium

---
+++ 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i-07-09-2026-23-39-49.java
@@ -0,0 +1,77 @@
+class Solution 
+{
+    public boolean[] pathExistenceQueries(int n, int[] nums, int maxDiff, int[][] queries) 
+    {
+        List<List<Integer>> adj=new ArrayList<>();
+        for(int i=0; i<n-1; i++)
+        {
+            adj.add(new ArrayList());
+            if(nums[i+1]-nums[i]<=maxDiff)
+            {
+                adj.get(i).add(i+1);
+            }
+        }
+        DSU dsu=new DSU(n);
+        for(int i=0; i<n-1; i++)
+        {
+            for(int no: adj.get(i))
+            {
+                dsu.unionBySize(i, no);
+            }
+        }
+
+        int m=queries.length;
+        boolean[] res=new boolean[m];
+        for(int i=0; i<m; i++)
+        {
+            int[] query=queries[i];
+            if(dsu.isSameParent(query[0], query[1])) res[i]=true;
+        }
+        return res;
+    }
+}
+class DSU
+{
+    int components;
+    int[] parent, size;
+    DSU(int n)
+    {
+        components=n;
+        parent=new int[n];
+        size=new int[n];
+        for(int i=0; i<n; i++)
+        {
+            size[i]=1;
+            parent[i]=i;
+        }
+    }
+    int findRootParent(int node)
+    {
+        if(node==parent[node]) return node;
+        return parent[node]=findRootParent(parent[node]);
+    }
+    boolean isSameParent(int node1, int node2)
+    {
+        int p1=findRootParent(node1);
+        int p2=findRootParent(node2);
+        return p1==p2;
+    }
+
+    void unionBySize(int node1, int node2)
+    {
+        int p1=findRootParent(node1);
+        int p2=findRootParent(node2);
+        if(p1==p2) return;
+        components--;
+        if(size[p1]<size[p2])
+        {
+            size[p2]+=size[p1];
+            parent[p1]=p2;
+        }
+        else
+        {
+            size[p1]+=size[p2];
+            parent[p2]=p1;
+        }
+    }
+}
