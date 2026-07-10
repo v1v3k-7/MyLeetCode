bd244fded193c03a74188e58be6e08add1d47c33
Vivek <248838127+v1v3k-7@users.noreply.github.com>
2026-07-10 22:04:26 +0530

    Time: 1 ms (69.78%), Space: 43.3 MB (7.81%) - Medium

---
+++ 0377-combination-sum-iv/0377-combination-sum-iv-07-10-2026-22-04-25.java
@@ -0,0 +1,27 @@
+class Solution 
+{
+    int n;
+    int[][] dp;
+    public int combinationSum4(int[] nums, int target) 
+    {
+        n=nums.length;
+        dp=new int[n][target+1];
+        for(int i=0; i<n; i++) Arrays.fill(dp[i], -1);
+
+        return backtrack(0, nums, target);
+    }
+
+    public int backtrack(int idx, int[] nums, int target)
+    {
+        if(target==0) return 1;
+        if(idx>=nums.length) return 0;
+
+        if(dp[idx][target]!=-1) return dp[idx][target];
+        int take=0;
+        if(nums[idx]<=target)
+            take=backtrack(0, nums, target-nums[idx]);
+        int noTake=backtrack(idx+1, nums, target); //increase index
+
+        return dp[idx][target]=take+noTake;
+    }
+}
