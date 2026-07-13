0a8072199b780d6c408372f079074acfff55b8c2
Vivek <248838127+v1v3k-7@users.noreply.github.com>
2026-07-13 23:22:34 +0530

    Time: 66 ms (26.09%), Space: 45.9 MB (92.49%) - Medium

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence-07-13-2026-23-22-34.java
@@ -0,0 +1,38 @@
+class Solution {
+    // DP Bottom-Up Approach
+    public int lengthOfLIS(int[] nums) 
+    {
+        int n=nums.length;
+        int[] dp=new int[n]; // dp[i] -> Length of LIS ending at index i
+        Arrays.fill(dp, 1); //sabka length abhi 1 hi hai
+        int maxLen=1;
+        for(int i=1; i<n; i++)
+        {
+            for(int j=0; j<i; j++)
+            {
+                if(nums[j]<nums[i])
+                {
+                    dp[i]=Math.max(dp[j]+1, dp[i]);
+                }
+                if(dp[i]>maxLen) maxLen=dp[i];
+            }
+        }
+        return maxLen;
+    }
+
+    // private int solve(int[] nums, int idx, int prev)
+    // {
+    //     if(idx==nums.length) return 0;
+    //     if(prev!=-1 && dp[prev][idx]!=-1) return dp[prev][idx];
+
+    //     int pick=0;
+    //     if(prev==-1 || nums[idx]>nums[prev])
+    //     {
+    //         pick=1+solve(nums, idx+1, idx);
+    //     }
+    //     int noPick=solve(nums, idx+1, prev);
+
+    //     if(prev!=-1) dp[prev][idx]=Math.max(pick, noPick);
+    //     return Math.max(pick, noPick);
+    // }
+}
