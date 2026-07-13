88cca73845c35b08b4546634742d8f7a7723cb14
Vivek <248838127+v1v3k-7@users.noreply.github.com>
2026-07-13 10:23:50 +0530

    Time: 149 ms (90.43%), Space: 44.7 MB (100%) - Hard

---
+++ 2014-longest-subsequence-repeated-k-times/2014-longest-subsequence-repeated-k-times-07-13-2026-10-23-50.java
@@ -0,0 +1,59 @@
+class Solution 
+{
+    String res="";
+    public String longestSubsequenceRepeatedK(String s, int k) 
+    {
+        int[] freq=new int[26];
+        for(char ch: s.toCharArray()) freq[ch-'a']++;
+
+        int[] useable=new int[26];
+        boolean[] canUse=new boolean[26];
+        for(int i=0; i<26; i++)
+        {
+            if(freq[i]>=k) 
+            {
+                canUse[i]=true;
+                useable[i]=freq[i]/k;
+            }
+        }
+
+        int maxLen=s.length()/k;
+
+        backtrack(s, new StringBuilder(), useable, canUse, k, maxLen);
+        return res;
+    }
+
+    private void backtrack(String s, StringBuilder sb, int[] useable, boolean[] canUse, int k, int maxLen)
+    {
+        if(sb.length()>maxLen) return;
+
+        for(int i=25; i>=0; i--)
+        {
+            if(!canUse[i] || useable[i]==0) continue;
+
+            sb.append((char)(i+'a'));
+            useable[i]--;
+
+            if (isSubsequence(s, sb, k)) {
+                if (sb.length() > res.length())
+                    res = sb.toString();
+
+                backtrack(s, sb, useable, canUse, k, maxLen);
+            }
+
+            sb.deleteCharAt(sb.length()-1);
+            useable[i]++;   
+        }
+    }
+    private boolean isSubsequence(String s, StringBuilder sub, int k)
+    {
+        int i=0, j=0;
+        int n=s.length(), m=sub.length();
+        while(i<n && j<m*k)
+        {
+            if(sub.charAt(j%m)==s.charAt(i)) j++;
+            i++;
+        }
+        return j==m*k;
+    }
+}
