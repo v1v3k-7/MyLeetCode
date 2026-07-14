e4df7fc2c03639716282ea3edb9e5ea9e584e0a4
Vivek <248838127+v1v3k-7@users.noreply.github.com>
2026-07-14 20:54:17 +0530

    Time: 3 ms (98.37%), Space: 47.4 MB (18.05%) - Medium

---
+++ 0072-edit-distance/0072-edit-distance-07-14-2026-20-54-16.java
@@ -0,0 +1,38 @@
+class Solution 
+{
+    // DP Top-Down Approach (Memo)
+    int n, m;
+    public int minDistance(String word1, String word2) 
+    {
+        n=word1.length();
+        m=word2.length();
+        int[][] dp=new int[n][m];
+        for(int i=0; i<n; i++) Arrays.fill(dp[i], -1);
+        return solve(n-1, m-1, word1, word2, dp);
+    }
+
+    private int solve(int i, int j, String s1, String s2, int[][] dp)
+    {
+        //if i OFB then remaining char of s2 will be add in s1 -> (m-j) operation, if j OFB then remaining char of s1 will get deleted(n-i operation) to make s1 from s2
+        if(i<0) return j+1; //insert all the chars left in j 
+        if(j<0) return i+1; //delete all the chars which still left in s1
+
+        if(dp[i][j]!=-1) return dp[i][j];
+
+        if(s1.charAt(i)==s2.charAt(j))
+        {
+            return dp[i][j]=0+solve(i-1, j-1, s1, s2, dp);
+        }
+        else
+        {
+            //insert 
+            int ins=1+solve(i, j-1, s1, s2, dp);
+            //delete
+            int del=1+solve(i-1, j, s1, s2, dp);
+            //replace
+            int rep=1+solve(i-1, j-1, s1, s2, dp);
+
+            return dp[i][j]=Math.min(ins, Math.min(del, rep));
+        }
+    }
+}
