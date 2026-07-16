97bec5283dc3fe4da5af73dc7e54e513fe5beb85
Vivek <248838127+v1v3k-7@users.noreply.github.com>
2026-07-16 08:47:32 +0530

    Time: 58 ms (41.5%), Space: 108.1 MB (63.5%) - Medium

---
+++ 3867-sum-of-gcd-of-formed-pairs/3867-sum-of-gcd-of-formed-pairs-07-16-2026-08-47-30.java
@@ -0,0 +1,27 @@
+class Solution {
+    public long gcdSum(int[] nums) 
+    {
+        int n=nums.length;
+        int mx=Integer.MIN_VALUE;
+        int[] arr=new int[n];
+        for(int i=0; i<n; i++)
+        {
+            if(nums[i]>mx) mx=nums[i];
+            arr[i]=findGCD(nums[i], mx);;
+        }
+        long res=0;
+        Arrays.sort(arr);
+        int i=0, j=n-1;
+        while(i<j)
+        {
+            res+=(long)findGCD(arr[j], arr[i]);;
+            i++;
+            j--;
+        }
+        return res;
+    }
+    private int findGCD(int a, int b)
+    {
+        return (b==0)?a:findGCD(b, a%b);
+    }
+}
