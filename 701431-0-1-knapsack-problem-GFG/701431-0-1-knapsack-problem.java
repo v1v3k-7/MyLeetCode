7dafb79c22c7f0c32fb7c6302e94082eed3ddf28
Vivek <248838127+v1v3k-7@users.noreply.github.com>
2026-07-17 09:33:24 +0530

    Bottom-Up (Space Optimal)

--- 701431-0-1-knapsack-problem-GFG/701431-0-1-knapsack-problem.java
+++ 701431-0-1-knapsack-problem-GFG/701431-0-1-knapsack-problem.java
@@ -2,31 +2,40 @@ class Solution {
     public int knapsack(int W, int val[], int wt[]) 
     {
         int n=val.length;
-        int[][] dp=new int[n][W+1]; //dp[i][j] -> max kitne val le skte hai agar 
-                                    // val[0:i] tak hi hai and max capacity(W) j ho to
-        for(int j=0; j<=W; j++)
+        // int[][] dp=new int[n][W+1]; //dp[i][j] -> max kitne val le skte hai agar 
+        //                             // val[0:i] tak hi hai and max capacity(W) j ho to
+        // for(int j=0; j<=W; j++)
+        // {
+        //     if(j<wt[0]) dp[0][j]=0;
+        //     else dp[0][j]=val[0]; //chahe kitna b max capacity jyada ho weight[0] ka value ek hi bar le skte
+        // }
+        // //and pure first column me b zero rhega kyuki W(j=0) hai yha to maxCap hi 0 hai to koi val aa hi nhi skta
+        
+        int[] maxVal=new int[W+1];
+        for(int i=0; i<=W; i++)
         {
-            if(j<wt[0]) dp[0][j]=0;
-            else dp[0][j]=val[0]; //chahe kitna b max capacity jyada ho weight[0] ka value ek hi bar le skte
+            if(i<wt[0]) maxVal[i]=0;
+            else maxVal[i]=val[0];
         }
-        //and pure first column me b zero rhega kyuki W(j=0) hai yha to maxCap hi 0 hai to koi val aa hi nhi skta
         
         for(int i=1; i<n; i++)
         {
+            int[] cur=new int[W+1];
             for(int j=1; j<=W; j++)
             {
                 int take=0;
                 if(wt[i]<=j) //maxCap(j) se km hoga weight tabi le skte
                 {
                     int remain=j-wt[i];
-                    take=val[i] + dp[i-1][remain];
+                    take=val[i] + maxVal[remain];
                 }
-                int notTake=dp[i-1][j];
-                dp[i][j]=Math.max(take, notTake);
+                int notTake=maxVal[j];
+                cur[j]=Math.max(take, notTake);
             }
+            maxVal=cur;
         }
         
-        return dp[n-1][W];
+        return maxVal[W];
         
     }
     // private int solve(int n, int W, int[] val, int[] wt, int[][] dp)
