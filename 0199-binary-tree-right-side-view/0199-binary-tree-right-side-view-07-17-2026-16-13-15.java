a2aadd9ea8cf0079c37c407c3bab149f203ed7c6
Vivek <248838127+v1v3k-7@users.noreply.github.com>
2026-07-17 16:13:16 +0530

    Time: 1 ms (71.7%), Space: 43.4 MB (84.89%) - Medium

---
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view-07-17-2026-16-13-15.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) 
+    {
+        List<Integer> res=new ArrayList<>();
+        if(root==null) return res;
+        Queue<TreeNode> queue=new LinkedList();
+        queue.offer(root);
+        while(!queue.isEmpty())
+        {
+            int size=queue.size();
+            for(int i=0; i<size; i++)
+            {
+                TreeNode node=queue.poll();
+                if(node.left!=null) queue.offer(node.left);
+                if(node.right!=null) queue.offer(node.right);
+                if(i==size-1) res.add(node.val);
+            }
+        }
+        return res;
+    }
+}
