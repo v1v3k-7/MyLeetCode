a5c2774ffb059c3f699862863b14e36e5c584199
Vivek <248838127+v1v3k-7@users.noreply.github.com>
2026-07-17 13:36:40 +0530

    Time: 1 ms (77.47%), Space: 43.1 MB (25.93%) - Medium

---
+++ 0165-compare-version-numbers/0165-compare-version-numbers-07-17-2026-13-36-38.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int compareVersion(String version1, String version2) 
+    {
+        String[] v1=version1.split("\\.");
+        String[] v2=version2.split("\\.");
+        int len1=v1.length, len2=v2.length;
+        int i=0, j=0;
+        while(i<len1 && j<len2)
+        {
+            int val1=Integer.parseInt(v1[i]);
+            int val2=Integer.parseInt(v2[j]);
+            if(val1>val2) return 1;
+            else if(val2>val1) return -1;
+            i++;
+            j++;
+        }
+        while(i<len1)
+        {
+            if(Integer.parseInt(v1[i])!=0) return 1;
+            i++;
+        }
+        while(j<len2)
+        {
+            if(Integer.parseInt(v2[j])!=0) return -1;
+            j++;
+        }
+        return 0;
+    }
+}
