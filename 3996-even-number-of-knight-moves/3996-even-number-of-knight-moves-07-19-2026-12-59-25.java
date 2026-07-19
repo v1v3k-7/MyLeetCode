47ab9bb50c0c57ece598cc7829ca4e141cc029ce
Vivek <248838127+v1v3k-7@users.noreply.github.com>
2026-07-19 12:59:26 +0530

    Time: 1 ms (76.35%), Space: 43.8 MB (91.22%) - Easy

---
+++ 3996-even-number-of-knight-moves/3996-even-number-of-knight-moves-07-19-2026-12-59-25.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean canReach(int[] start, int[] target) 
+    {
+        //start or target ka color same hai mtlb ki wo even move kie, if alag hai mtlb ki wo odd move kia
+        // (i+j) -> even mean black color hoga wo cell, odd hua to white color
+        return ((start[0]+start[1])&1) == ((target[0]+target[1])&1);
+    }
+}
