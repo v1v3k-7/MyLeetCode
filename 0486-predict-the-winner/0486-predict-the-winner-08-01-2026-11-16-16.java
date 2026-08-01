0e2be0ee95403c0e16b51de5cc7177081d97cd29
Vivek <248838127+v1v3k-7@users.noreply.github.com>
2026-08-01 11:16:18 +0530

    Time: 0 ms (100%), Space: 43 MB (23.56%) - Medium

---
+++ 0486-predict-the-winner/0486-predict-the-winner-08-01-2026-11-16-16.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean predictTheWinner(int[] nums) 
+    {
+        int n=nums.length;
+        Integer[][] dp=new Integer[n][n];
+        return solve(0, n-1, nums, dp)>=0;
+    }
+    private int solve(int left, int right, int[] nums, Integer[][] dp) //player - 0 (1st player), 1 (2nd player)
+    {
+        if(left==right)
+        {
+            return nums[left];
+        }
+        if(dp[left][right]!=null) return dp[left][right];
+
+        int pickLeft=nums[left] - solve(left+1, right, nums, dp);
+        int pickRight=nums[right] - solve(left, right-1, nums, dp);
+        
+        return dp[left][right]=Math.max(pickLeft, pickRight);
+    }
+}
